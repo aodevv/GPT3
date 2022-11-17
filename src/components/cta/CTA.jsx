90415e44e697cd66f55a62786325154d11f227f4
l3edssa <aimarah.oussam@gmail.com>
2022-11-17 23:26:19 +0100

    finished CTA section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./CTA.css";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="gpt3__cta section__margin">
+      <div className="gpt3__cta-heading">
+        <p>Request Early Access to Get Started</p>
+        <h2>Register today & start exploring the endless possiblities.</h2>
+      </div>
+      <div className="gpt3__cta-button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
